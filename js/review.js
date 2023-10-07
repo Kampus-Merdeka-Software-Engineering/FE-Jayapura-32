12b6b62b796dbb63eda7ad2eac7c3236ff296f99
Ilham <137876342+cuplis22@users.noreply.github.com>
2023-10-07 17:49:01 +0700

    Add files via upload

---
+++ js/review.js
@@ -0,0 +1,91 @@
+const allStar = document.querySelectorAll(".rating .star");
+const ratingValue = document.querySelector(".rating input");
+var nameError = document.getElementById("name-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+allStar.forEach((item, idx) => {
+    item.addEventListener("click", function() {
+        let click = 0;
+        ratingValue.value = idx + 1;
+        console.log(ratingValue.value);
+
+        allStar.forEach((i) => {
+            i.classList.replace("bxs-star", "bx-star");
+            i.classList.remove("active");
+        });
+        for (let i = 0; i < allStar.length; i++) {
+            if (i <= idx) {
+                allStar[i].classList.replace("bx-star", "bxs-star");
+                allStar[i].classList.add("active");
+            } else {
+                allStar[i].style.setProperty("--i", click);
+                click++;
+            }
+        }
+    });
+});
+
+// Submit
+
+function validateName() {
+    var name = document.getElementById("review-name").value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = "Name is required";
+        return false;
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "Write full name";
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="bx bx-check check"></i>';
+    return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById("review-email").value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = "Email is required";
+        return false;
+    }
+
+    if (!email.match(/^[A-Za-z\\._\-[0-9]*[@][A-Za-z]*[\\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "Email Invalid";
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="bx bx-check check"></i>';
+    return true;
+}
+
+function validateMessage() {
+    var message = document.getElementById("review-message").value;
+    var required = 10;
+    var left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + "more characters required";
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="bx bx-check check"></i>';
+    return true;
+}
+
+// eslint-disable-next-line no-unused-vars
+function validateForm() {
+    if (!(!validateName() || !validateEmail() || !validateMessage())) {
+        return;
+    }
+    submitError.style.display = "block";
+    submitError.innerHTML = "Please fix error to submit";
+    setTimeout(function() {
+        submitError.style.display = "none";
+    }, 3000);
+    return false;
+}
---
+++ js/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+
+/**
+ * addEvent on element
+ */
+
+const addEventOnElem = function(elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    navbarToggler.classList.toggle("active");
+}
+
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function() {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzP_fB7ZNFsvIppxkTguhGtZv5V69fPhb175-AR8GVxyUquJmyW1eUyHdZgk59jeAmOdg/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        // eslint-disable-next-line no-unused-vars
+        .then(response => {
+            msg.innerHTML = "Thank You For Subscribing!"
+            setTimeout(function() {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+});
